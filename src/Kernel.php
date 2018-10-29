914853092a2d6b24f64db26cce1290dfd1bdf48c
Jonathan Doydora <jonathan.doydora@godigitalcorp.com>
2018-10-29 14:40:39 +0800

    added htaccess; added dev framework.yaml for changing cache type in dev environment;

--- src/Kernel.php
+++ src/Kernel.php
@@ -37,25 +37,44 @@ class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
     {
-        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
-        // Feel free to remove the "container.autowiring.strict_mode" parameter
-        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
+        // $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
+        // // Feel free to remove the "container.autowiring.strict_mode" parameter
+        // // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
+        // $container->setParameter('container.autowiring.strict_mode', true);
+        // $container->setParameter('container.dumper.inline_class_loader', true);
+        // $confDir = $this->getProjectDir().'/config';
+
+        // $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
+        // $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
+        // $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
+        // $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+
         $container->setParameter('container.autowiring.strict_mode', true);
         $container->setParameter('container.dumper.inline_class_loader', true);
         $confDir = $this->getProjectDir().'/config';
-
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/packages/*'.self::CONFIG_EXTS, 'glob');
+        if (is_dir($confDir.'/packages/'.$this->environment)) {
+            $loader->load($confDir.'/packages/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
+        }
+        $loader->load($confDir.'/services'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/services_'.$this->environment.self::CONFIG_EXTS, 'glob');
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes)
     {
-        $confDir = $this->getProjectDir().'/config';
+        // $confDir = $this->getProjectDir().'/config';
 
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+        // $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
+        // $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
+        // $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+
+        $confDir = $this->getProjectDir().'/config';
+        if (is_dir($confDir.'/routes/')) {
+            $routes->import($confDir.'/routes/*'.self::CONFIG_EXTS, '/', 'glob');
+        }
+        if (is_dir($confDir.'/routes/'.$this->environment)) {
+            $routes->import($confDir.'/routes/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
+        }
+        $routes->import($confDir.'/routes'.self::CONFIG_EXTS, '/', 'glob');
     }
 }
