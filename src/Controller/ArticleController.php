6cc0ef7c87fd1d911215591c2624810e9467908a
Jonathan Doydora <jonathan.doydora@godigitalcorp.com>
2018-10-29 15:07:47 +0800

    added service MarkdownHelper; dependency injected through constructor of MarkdownHelper;

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller;
 
-use Michelf\MarkdownInterface;
+use App\Service\MarkdownHelper;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
@@ -27,7 +26,7 @@ class ArticleController extends AbstractController
 	/**
 	 * @Route("/news/{slug}", name="article_show")
 	 */
-	public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
+	public function show($slug, MarkdownHelper $markdownHelper)
 	{
 		$comments = array(
 			'I like rainbows',
@@ -65,13 +64,7 @@ belly tongue alcatra, shoulder excepteur in beef bresaola duis ham bacon eiusmod
 adipisicing cow cillum tenderloin.
 EOF;
 
-		$item = $cache->getItem('markdown_'.md5($articleContent));
-		if ( ! $item->isHit()) {
-			$item->set($markdown->transform($articleContent));
-			$cache->save($item);
-		}
-
-		$articleContent = $item->get();
+		$articleContent = $markdownHelper->parse($articleContent);
 
 		return $this->render('article/show.html.twig', array(
 			'title' => ucwords(str_replace('-', ' ', $slug)),
